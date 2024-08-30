77008e9750a8f0457f173bb2060cf46007c7a47a
Kevin Best <tkevinbest@gmail.com>
2024-08-30 17:37:40 +0100

    Updates to set control gains.

--- BNO055_demo.py
+++ BNO055_demo.py
@@ -1,7 +1,7 @@
 from opensourceleg.sensors.imu import BNO055
 from opensourceleg.time.time import SoftRealtimeLoop
 from opensourceleg.control.compiled_controller import CompiledController
-from opensourceleg.actuators.dephy import DephyActuator, CONTROL_MODES
+from opensourceleg.actuators.dephy import DephyLegacyActuator, CONTROL_MODES
 from pathlib import Path
 
 controller = CompiledController(library_name="IMU_Follower",
@@ -16,20 +16,21 @@ controller.define_outputs([('knee_angle_rad',controller.types.c_double)])
 imu = BNO055()
 imu.start()
 
-# knee = DephyActuator(tag="Knee", gear_ratio=9*83/18, offline=True)
-# with knee:
-#     knee.home()
-#     knee.set_control_mode(CONTROL_MODES.POSITION)
-for t in SoftRealtimeLoop(1/100, True):
-    imu.update()
-    controller.inputs.accel.x = imu.acc_x
-    controller.inputs.accel.y = imu.acc_y
-    controller.inputs.accel.z = imu.acc_z
-    controller.inputs.gyro.x = imu.gyro_x
-    controller.inputs.gyro.y = imu.gyro_y
-    controller.inputs.gyro.z = imu.gyro_z
-    controller.run()
-    print(controller.outputs.knee_angle_rad)
-        # knee.set_output_position(controller.outputs.knee_angle_rad)
+knee = DephyLegacyActuator(tag="Knee", gear_ratio=9, offline=False)
+with knee:
+    knee.set_motor_zero_position(knee.motor_position)
+    knee.set_control_mode(CONTROL_MODES.POSITION)
+    knee.set_position_gains()
+    for t in SoftRealtimeLoop(1/100, True):
+        imu.update()
+        controller.inputs.accel.x = imu.acc_x
+        controller.inputs.accel.y = imu.acc_y
+        controller.inputs.accel.z = imu.acc_z
+        controller.inputs.gyro.x = imu.gyro_x
+        controller.inputs.gyro.y = imu.gyro_y
+        controller.inputs.gyro.z = imu.gyro_z
+        controller.run()
+        print(imu.acc_z, controller.outputs.knee_angle_rad)
+        knee.set_output_position(controller.outputs.knee_angle_rad)
 
 imu.stop()
